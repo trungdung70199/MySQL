55d872a016d23daa31e6e29d43e544b11a02c6bd
trungdung70199 <trungdung70199@gmail.com>
2023-06-19 09:45:21 +0900

    update

--- basic.sql
+++ basic.sql
@@ -1,2 +1,12 @@
 -- user table
 SELECT * FROM users;
+
+-- from users table, choose the id = 1 
+SELECT * FROM users WHERE id  = 1;
+
+-- From users table if id diff 1
+SELECT * FROM users WHERE id != 1;
+
+-- From users table if id >= 50
+SELECT * FROM users WHERE id >= 50;
+
